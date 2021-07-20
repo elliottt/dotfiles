89aee6b6386b99ad71c1efe9209397538592a4a2
Trevor Elliott <awesomelyawesome@gmail.com>
2021-07-19 21:18:25 -0700

    Better sorbet config

--- configs/neovim/config/nvim/lua/lsp.lua
+++ configs/neovim/config/nvim/lua/lsp.lua
@@ -11,44 +11,63 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     }
 )
 
+-- setup sorbet
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+configs.sorbet = {
+    default_config = {
+        cmd = { "srb", "tc", "--lsp" },
+        filetypes = { "ruby" },
+
+        -- The order here is important for not accidentally walking up to the root of the sorbet
+        -- repo
+        root_dir = util.root_pattern('Gemfile', '.git'),
+    }
+}
+
 return {
     setup = function(mappings)
+        local home = vim.fn.environ().HOME
+
         -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
         local sumneko_root_path = vim.fn.environ().HOME .. '/Source/lua-language-server'
-        local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+        if vim.fn.isdirectory(sumneko_root_path) then
+            local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
 
-        local runtime_path = vim.split(package.path, ';')
-        table.insert(runtime_path, "lua/?.lua")
-        table.insert(runtime_path, "lua/?/init.lua")
+            local runtime_path = vim.split(package.path, ';')
+            table.insert(runtime_path, "lua/?.lua")
+            table.insert(runtime_path, "lua/?/init.lua")
 
-        lsp.sumneko_lua.setup {
-          on_attach = mappings.lsp_attach,
-          cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-          settings = {
-            Lua = {
-              runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path,
+            lsp.sumneko_lua.setup {
+              on_attach = mappings.lsp_attach,
+              cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+              settings = {
+                Lua = {
+                  runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                    -- Setup your lua path
+                    path = runtime_path,
+                  },
+                  diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = {'vim'},
+                  },
+                  workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                  },
+                  -- Do not send telemetry data containing a randomized but unique identifier
+                  telemetry = {
+                    enable = false,
+                  },
+                },
               },
-              diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
-              },
-              workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-              },
-              -- Do not send telemetry data containing a randomized but unique identifier
-              telemetry = {
-                enable = false,
-              },
-            },
-          },
-        }
+            }
+        end
 
-        -- when clangd is present in the bazel sandbox, prefer that
+        -- clangd config
         local clangd_opts = {
             on_attach = mappings.lsp_attach,
         }
@@ -58,31 +77,27 @@ return {
         end
         lsp.clangd.setup(clangd_opts)
 
-        lsp.rust_analyzer.setup {
+        -- sorbet config
+        local sorbet_opts = {
             on_attach = mappings.lsp_attach,
         }
 
         if vim.fn.glob("scripts/bin/typecheck") ~= "" then
-            -- setup sorbet
-            local configs = require 'lspconfig/configs'
-            local util = require 'lspconfig/util'
-
-            local root_pattern = util.root_pattern('.git', 'Gemfile')
-
-            configs.sorbet = {
-                default_config = {
-                    cmd = { "pay", "exec", "scripts/bin/typecheck", "--lsp" },
-                    filetypes = { "ruby" },
-                    root_dir = function(name)
-                        return root_pattern(name)
-                    end,
-                }
-            }
-
-            lsp.sorbet.setup {
-                on_attach = mappings.lsp_attach,
-            }
+            -- use 'pay exec' when in pay-server
+            sorbet_opts.cmd = { "pay", "exec", "scripts/bin/typecheck", "--lsp" }
+        else
+            local local_sorbet_build = vim.fn.glob(home.."/stripe/sorbet/bazel-bin/main/sorbet")
+            if local_sorbet_build ~= "" then
+                -- prefer a local build of sorbet if it's available
+                sorbet_opts.cmd = { local_sorbet_build, "--lsp", "--silence-dev-message" }
+            end
         end
+        lsp.sorbet.setup(sorbet_opts)
+
+        -- rust-analyzer config
+        lsp.rust_analyzer.setup {
+            on_attach = mappings.lsp_attach,
+        }
 
     end
 }
