88206f38457d5bd850e8a992185b5f7cff147b64
Trevor Elliott <awesomelyawesome@gmail.com>
2022-01-18 10:00:18 -0800

    Switch to c-n for autocompletion

--- configs/neovim/config/nvim/lua/mappings.lua
+++ configs/neovim/config/nvim/lua/mappings.lua
@@ -74,7 +74,7 @@ cmp.setup{
     },
 
     mapping = {
-        ['<c-space>'] = cmp.mapping(cmp.mapping.complete(), { 'i' }),
+        ['<c-n>'] = cmp.mapping(cmp.mapping.complete(), { 'i' }),
         ['<cr>'] = cmp.mapping.confirm{ select = true },
     },
 
