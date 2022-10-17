71895d9eda600139d4b3b7fabe0a0a89cc6e8f17
Trevor Elliott <awesomelyawesome@gmail.com>
2022-10-17 11:42:48 -0700

    Fix lsp completion compatibility

--- configs/neovim/config/nvim/lua/lsp.lua
+++ configs/neovim/config/nvim/lua/lsp.lua
@@ -78,7 +78,7 @@ LspUtil = {
     },
 }
 
-local capabilities = require 'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require 'cmp_nvim_lsp'.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 return {
     setup = function(mappings)
