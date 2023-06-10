d5d222779842ce928d8174d38f5cf99286a05c55
Trevor Elliott <awesomelyawesome@gmail.com>
2023-06-10 10:57:46 -0700

    Remove the sumneko config

--- configs/neovim/config/nvim/lua/lsp.lua
+++ configs/neovim/config/nvim/lua/lsp.lua
@@ -84,44 +84,6 @@ return {
     setup = function(mappings)
         local home = vim.fn.environ().HOME
 
-        -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-        local sumneko_root_path = vim.fn.environ().HOME .. '/Source/lua-language-server'
-        if vim.fn.isdirectory(sumneko_root_path) ~= 0 then
-            local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
-
-            local runtime_path = vim.split(package.path, ';')
-            table.insert(runtime_path, "lua/?.lua")
-            table.insert(runtime_path, "lua/?/init.lua")
-
-            lsp.sumneko_lua.setup {
-              on_attach = mappings.lsp_attach,
-              cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-              capabilities = capabilities,
-              settings = {
-                Lua = {
-                  runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT',
-                    -- Setup your lua path
-                    path = runtime_path,
-                  },
-                  diagnostics = {
-                    -- Get the language server to recognize the `vim` global
-                    globals = {'vim'},
-                  },
-                  workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true),
-                  },
-                  -- Do not send telemetry data containing a randomized but unique identifier
-                  telemetry = {
-                    enable = false,
-                  },
-                },
-              },
-            }
-        end
-
         -- clangd config
         local clangd_path = vim.fn.glob('bazel-*/external/llvm_toolchain*/bin/clangd')
         if clangd_path ~= "" then
