d0c784c8be32cb63c81eff4f473b6a72fb42ebe7
Trevor Elliott <awesomelyawesome@gmail.com>
2023-07-17 14:32:39 -0700

    Update nvim config and add a home-manager one

--- configs/neovim/config/nvim/lua/plugins.lua
+++ configs/neovim/config/nvim/lua/plugins.lua
@@ -28,11 +28,7 @@ return require 'packer'.startup{function(use)
 
     use {
         'nvim-telescope/telescope-fzf-native.nvim',
-        run = [[
-        cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && \
-        cmake --build build --config Release && \
-        cmake --install build --prefix build
-        ]]
+        run = "make",
     }
 
     use {
