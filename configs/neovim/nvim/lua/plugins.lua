8551ac9526829c37c2d6d7169f27432db526f879
Trevor Elliott <awesomelyawesome@gmail.com>
2023-07-21 10:07:16 -0700

    No bg color for the gutter in nvim

--- configs/neovim/nvim/lua/plugins.lua
+++ configs/neovim/nvim/lua/plugins.lua
@@ -121,7 +121,18 @@ return require 'packer'.startup{function(use)
         'rebelot/kanagawa.nvim',
         disable = false,
         config = function()
-            require 'kanagawa'.setup{ transparent = true }
+            require 'kanagawa'.setup{
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = 'none',
+                            },
+                        },
+                    },
+                },
+                transparent = true,
+            }
             vim.cmd.colorscheme('kanagawa')
         end
     }
