5f4678b3797f1cd768e96add2827a513f5bfffdc
Trevor Elliott <awesomelyawesome@gmail.com>
2023-09-30 18:44:08 -0700

    Add some comments

--- configs/zsh/home.nix
+++ configs/zsh/home.nix
@@ -51,8 +51,10 @@
 
   };
 
+  # If direnv is enabled, make sure that zsh integration is present.
   programs.direnv.enableZshIntegration = true;
 
+  # Enable fzf integration when zsh is managed by home-manager
   programs.fzf.enableZshIntegration = true;
 
 }
