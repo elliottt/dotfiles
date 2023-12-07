265fecf364edc54bccd01399174490cd5a54167d
Trevor Elliott <awesomelyawesome@gmail.com>
2023-12-06 16:43:52 -0800

    Update wit-bindgen to 0.16.0

--- packages/wit-bindgen.nix
+++ packages/wit-bindgen.nix
@@ -2,22 +2,18 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "wit-bindgen-cli";
-  version = "0.12.0";
+  version = "0.16.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "bytecodealliance";
     repo = "wit-bindgen";
     rev = "${pname}-${version}";
-    sha256 = "sha256-Hk9ENKkXB86bbtRE6i6bdA2gFGP566A+feHxZCzvw+U=";
+    sha256 = "sha256-QqLTXvzBobDsdwo30yUFK2bHedawiYPni2zhKk6I7j8=";
   };
 
   doCheck = false;
 
-  postPatch = ''
-    cp ${./wit-bindgen.lock} Cargo.lock
-  '';
-
   cargoLock = {
-    lockFile = ./wit-bindgen.lock;
+    lockFile = "${src}/Cargo.lock";
   };
 }
