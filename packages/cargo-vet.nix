f69240e1dd6fa5bcba2add67c1cf014ce6c9eaf3
Trevor Elliott <awesomelyawesome@gmail.com>
2024-02-28 10:32:18 -0800

    Bump cargo-vet to 0.9.1

--- packages/cargo-vet.nix
+++ packages/cargo-vet.nix
@@ -2,13 +2,13 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "cargo-vet";
-  version = "0.8.0";
+  version = "0.9.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "mozilla";
     repo = "cargo-vet";
-    rev = "8c8b6d7a5237544c613de616a031586587f49a42";
-    sha256 = "sha256-WBoPDF69O4jhBvsETnFyGCcUGcaVbsRwUfIJoRt1mk8=";
+    rev = "5bd670061a22074eb5ee9d69feccc5900df00ec3";
+    sha256 = "sha256-676hoawqU3KTf2TnHdVAdTEgK4NLIOb4zHTmEYm1d1E=";
   };
 
   doCheck = false;
