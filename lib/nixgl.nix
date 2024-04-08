d9d70cb502d39d9def657cc368b219de26d7e3c7
Trevor Elliott <awesomelyawesome@gmail.com>
2024-04-08 09:34:21 -0700

    Make sure that the nixGL wrapper forwards the underlying package version

--- lib/nixgl.nix
+++ lib/nixgl.nix
@@ -4,6 +4,7 @@ binary: drv: pkgs.symlinkJoin {
   name = "${drv.name}-nixglwrapped";
   paths = [ drv ];
   nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+  version = drv.version;
   postBuild = ''
     makeBinaryWrapper \
       "${nixGL.auto.nixGLDefault}/bin/nixGL" \
