e89839afdab2acc3ac56eee587a65dfab96abfb4
Trevor Elliott <awesomelyawesome@gmail.com>
2024-10-23 13:51:02 -0700

    Pare down stripe config

--- hosts/st-trevor1.nix
+++ hosts/st-trevor1.nix
@@ -36,20 +36,12 @@
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   programs.zsh.initExtra = ''
-    ### BEGIN STRIPE
-    # All Stripe related shell configuration
-    # is at ~/.stripe/shellinit/zshrc and is
-    # persistently managed by Chef. You shouldn't
-    # remove this unless you don't want to load
-    # Stripe specific shell configurations.
-    #
-    # Feel free to add your customizations in this
-    # file (~/.zshrc) after the Stripe config
-    # is sourced.
-    if [[ -f ~/.stripe/shellinit/zshrc ]]; then
-      source ~/.stripe/shellinit/zshrc
-    fi
-    ### END STRIPE
+    # From stripe IT department
+    export PATH="$HOME/stripe/henson/bin:$PATH"
+    export PATH="$PATH:$HOME/stripe/space-commander/bin"
+    source "$HOME/.rbenvrc"
+    rbenv rehash
+    eval "$(nodenv init -)"
   '';
 
   home.file = {
